7f45b5746eb1163f9068a1b87b9f1bfc598289f0
connor-lynchh <cowlynch@ucsc.edu>
2023-06-06 13:54:41 -0700

    everything goes to somewhere now

---
+++ scene-flow.js
@@ -0,0 +1,28 @@
+class Intro extends Phaser.Scene {
+    constructor(){
+        super('intro');
+    }
+    preload(){
+
+        
+    }
+    create() {
+        this.add.text(610,50, "Scene flow").setFontSize(100);
+        this.add.text(0,500, "Scene flow stuff \n goes here").setFontSize(150);
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('intro'));
+        });
+    }
+}
+
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    scene: [Intro],
+    title: "scene flow",
+});
